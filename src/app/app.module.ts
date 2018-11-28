f65982b5833be49248fa996d0bf71153d76106c5
Liad Rinot <Liad.Rinot@clicktale.com>
2018-11-28 18:16:07 +0200

    Completed the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 // Components and Services:
 import { AppComponent } from './app.component';
@@ -11,6 +12,12 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component'
+
+const appRoutes:Routes = [
+  {path: 'products', component: ProductListComponent },
+  {path: 'product/:id', component: ProductPageComponent},
+  {path: '', redirectTo: 'products', pathMatch: 'full'}
+];
 
 @NgModule({ 
   declarations: [ // Components, Directives, and Pipes that belong to this module
@@ -23,7 +30,8 @@ import { ProductListComponent } from './product-list/product-list.component'
   imports: [ // ngModules used for general functionality
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     ProductService
